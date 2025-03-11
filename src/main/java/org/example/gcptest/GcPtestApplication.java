8f8a373ced695bdbe67995d65dd0b1ba316466bb
웃기진않아 <dugudada01@naver.com>
2025-03-11 19:24:38 +0900

    init

---
+++ src/main/java/org/example/gcptest/GcPtestApplication.java
@@ -0,0 +1,13 @@
+package org.example.gcptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcPtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcPtestApplication.class, args);
+	}
+
+}
